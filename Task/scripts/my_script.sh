b8cfaa984b18cf8fd621de9346dd62ef91859b4d
JakubAleksander <qbasawicki@wp.pl>
2020-08-27 20:12:06 +0200

    Changed the IP return command

--- Task/scripts/my_script.sh
+++ Task/scripts/my_script.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo '3D Lab'
-ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
-
+ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'
